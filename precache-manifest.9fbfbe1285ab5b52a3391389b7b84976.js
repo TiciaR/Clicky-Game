093a4d8d2f96b84c099347358345ffb9573aa14a
Ticia <ticiarodela@gmail.com>
2020-06-22 14:27:28 -0700

    Updates

---
+++ precache-manifest.9fbfbe1285ab5b52a3391389b7b84976.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11ed67a49a1a56fc94712bdab0aaeb3",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "1170fb5b761a3fd8b407",
+    "url": "/Clicky-Game/static/css/main.e5c5f1cd.chunk.css"
+  },
+  {
+    "revision": "f7b623742bd53144fd14",
+    "url": "/Clicky-Game/static/js/2.320f203f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.320f203f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1170fb5b761a3fd8b407",
+    "url": "/Clicky-Game/static/js/main.e0ea8cf4.chunk.js"
+  },
+  {
+    "revision": "f70336978153cff784ba",
+    "url": "/Clicky-Game/static/js/runtime-main.a0ce3ec2.js"
+  }
+]);
